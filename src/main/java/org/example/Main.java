4ec4403b51ce44d720028edf80cec411b05cdc5e
kirkhammett <@kirky359>
2025-02-05 18:40:01 +0200

    lab 1 po

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,31 @@
 package org.example;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-       long start = System.currentTimeMillis();
+       Random random = new Random();
 
+       long start = System.currentTimeMillis();
        int x = random.nextInt(400) +1;
-       int matrix[][] = new int [x][x];
+       int[][] matrix = new int [x][x];
 
        for(int i =0; i < x; i++){
            for(int k = 0; k<x; k++){
                matrix[i][k] = random.nextInt(10);
            }
        }
-
-       long finish = System.currentTimeMillis();
-       long time = finish - start;
        sum(matrix);
+       long finish = System.currentTimeMillis();
+
+       long time = finish - start;
+
        printMatrix(matrix);
        System.out.println("Start time: " + start);
        System.out.println("Finish time: " + finish);
        System.out.println("Execution time: " + time + " ms");
     }
+
 
     public static void sum(int[][] matrix){
         int size = matrix.length;
@@ -43,4 +46,6 @@ public class Main {
         }
     }
 }
+
+
 
